48ec7624dc3302dc7a967679f98229c371524422
adayush <adayush0@gmail.com>
2023-03-02 16:58:44 +0530

    fixed mobile min card height

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { LikeIcon } from './Icons'
 
 function Card({ image, showPopup }) {
-    return <div onClick={() => showPopup(image.id)} className="min-h-[250px] mb-1 cursor-pointer rounded-md overflow-hidden md:mb-5">
+    return <div onClick={() => showPopup(image.id)} className="min-h-[150px] md:min-h-[250px] mb-1 cursor-pointer rounded-md overflow-hidden md:mb-5">
         <img src={`${window.innerWidth >= 768 ? image.urls.small : image.urls.thumb}`} loading="lazy" className="w-full object-contain" />
         <div className="flex gap-2 p-2 justify-between border-[1px] dark:bg-black border-gray-200 dark:border-none rounded-b-md">
           <div className="flex gap-1 md:gap-2 align-middle">

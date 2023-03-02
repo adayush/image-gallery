a0409938ff56ff4c9b87484aa05e598ce1057ae9
adayush <adayush0@gmail.com>
2023-03-02 09:16:24 +0530

    added Access-Control-Allow-Origin header

--- src/hooks/useListPhotos.js
+++ src/hooks/useListPhotos.js
@@ -1,9 +1,12 @@
 function useListPhotos(query, page = 1, imageData, setImageData) {
-  const URL = query ? `http://api.unsplash.com/search/photos?query=${query}&` : "http://api.unsplash.com/photos?"
+  const URL = query ? `https://api.unsplash.com/search/photos?query=${query}&` : "https://api.unsplash.com/photos?"
   
   fetch(`${URL}page=${page}&client_id=${import.meta.env.VITE_CLIENT_ID}`, {
     method: 'GET',
-    mode: 'cors'
+    mode: 'cors',
+    headers: {
+      'Access-Control-Allow-Origin':'*',
+    },
   })
     .then(response => response.json())
     .then(data => data.results || data)

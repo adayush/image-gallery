0dc1d633caae2385adc18cb0f5250b7acc655323
adayush <adayush0@gmail.com>
2023-03-02 10:16:46 +0530

    fixed API string https

--- src/hooks/useGetPhoto.js
+++ src/hooks/useGetPhoto.js
@@ -1,7 +1,7 @@
 function useGetPhoto(imageId, setImageData) {  
   if (!imageId) return null;
 
-  fetch(`http://api.unsplash.com/photos/${imageId}?client_id=${import.meta.env.VITE_CLIENT_ID}`, {
+  fetch(`https://api.unsplash.com/photos/${imageId}?client_id=${import.meta.env.VITE_CLIENT_ID}`, {
     method: 'GET',
     mode: 'cors',
     headers: {
